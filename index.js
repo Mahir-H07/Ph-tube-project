ef44b79f5776beb4a52b0e8d950bf8e46d28d9d5
Mahir-H07 <ttxmahir@gmail.com>
2025-10-06 20:55:39 +0600

    data dynamic

---
+++ index.js
@@ -0,0 +1,90 @@
+
+
+
+
+
+
+function loadCategories() {
+    fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+        .then((res) => res.json())
+        .then((data) => displayCategories(data.categories));
+}
+
+
+
+
+function loadVideos(){
+    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((response)=>response.json())
+    .then((data)=> displayVideos(data.videos));
+}
+
+
+
+function displayCategories(categories) {
+
+    const categoryContainer = document.getElementById("category-container");
+
+    for (let cat of categories) {
+        // console.log(cat)
+
+        const categoryDiv = document.createElement("div");
+
+
+        categoryDiv.innerHTML = `
+         <button class="btn btn-sm hover:bg-red-600 hover:text-white">${cat.category}</button>
+        `;
+
+
+        categoryContainer.append(categoryDiv);
+    }
+
+}
+
+
+const displayVideos=(Videos)=>{
+
+const videoContainer= document.getElementById("video-container");
+
+Videos.forEach((video)=>{
+    console.log(video);
+
+    const videoCard = document.createElement("div");
+    videoCard.innerHTML = `
+       <div class="card bg-base-100">
+  <figure class="relative">
+    <img class="w-full h-[150px] object-cover"
+      src="${video.thumbnail}"
+      alt="Shoes" />
+     <span class="absolute bottom-2 right-2 text-white bg-black px-2 text-sm rounded ">3hrs 56 min ago</span>
+  </figure>
+  <div class="flex gap-3 px-1 py-5">
+    <div class="profile">
+        <div class="avatar">
+  <div class="ring-primary ring-offset-base-100 w-6 rounded-full ring-2 ring-offset-2">
+    <img src="${video.authors[0].profile_picture}" />
+  </div>
+</div>
+    </div>
+    <div class="intro">
+        <h2 class="text-sm font-semibold">Midnight Serenade</h2>
+        <p class="text-sm text-gray-400 flex gap-1"> 
+        ${video.authors[0].profile_name}
+        <img class="w-5 h-5" src="https://img.icons8.com/?size=48&id=QMxOVe0B9VzG&format=png" alt=""></p>
+        <p class="text-sm text-gray-400">${video.others.views}</p>
+    </div>
+    
+    
+   
+  </div>
+</div>
+     `
+     videoContainer.append(videoCard)
+})
+
+
+}
+
+
+loadCategories();
+loadVideos();
